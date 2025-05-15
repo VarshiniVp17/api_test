49ed53a0387b4ccfa9f36c177f4ac8ad06eb08e9
Varshini Prakash <varshini.prakash@srmtech.com>
2025-05-15 08:40:53 +0530

    Update welcome message

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Varshini\'s API!');
+  res.send('Hello my dear husbanddd❤️❤️❤️❤️');
 });
 
 app.get('/greet', (req, res) => {
-  res.json({ message: 'Hi, this is a public API by Varshini!' });
+  res.json({ message: 'Finally deployed my api🥹🥹 done my punishment😌😌😌' });
 });
 
 app.listen(PORT, () => {
