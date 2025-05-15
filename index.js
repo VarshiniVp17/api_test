f86c2045bea781acf72ae0e1d06ba24ba77a3bd4
Varshini Prakash <varshini.prakash@srmtech.com>
2025-05-15 09:32:08 +0530

    Update

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
-const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(cors()); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello my dear husbanddd❤️❤️❤️❤️');
+  res.send('Finally deployed my api🥹🥹');
 });
 
 app.get('/greet', (req, res) => {
-  res.json({ message: 'Finally deployed my api🥹🥹 done my punishment😌😌😌' });
+  res.json({ message: 'testttt' });
 });
+
+app.post('/greet', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).json({ message: 'Name is required!' });
+    }
+    res.json({ message: `Hello ${name}! This is Varshini’s API.` });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
